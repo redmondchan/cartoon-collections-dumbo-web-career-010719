5e4a90024aa56c7a23707f6b8c40010e0e4d6920
Redmond Chan <redmondchan92@gmail.com>
2019-01-06 18:20:36 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def long_planeteer_calls(array)# code an argument here
   array.any?{|word| word.length > 4}# Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{|cheese| cheese_types.include?(cheese)}
 end
